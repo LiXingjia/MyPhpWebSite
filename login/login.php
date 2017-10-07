e03fa6cc9654c821cbbc0f6007430c8f4a867bbe
lixingjia <873642712@qq.com>
2017-10-07 23:38:15 +0800

    第二次提交

--- login/login.php
+++ login/login.php
@@ -1,3 +1,11 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" 
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>用户登录</title>
+</head>
+<body>
 <?php
 session_start();
 
@@ -32,3 +40,5 @@ if($result = mysql_fetch_array($check_query)){
     exit('登录失败！点击此处 <a href="javascript:history.back(-1);">返回</a> 重试');
 }
 ?>
+</body>
+</html>
