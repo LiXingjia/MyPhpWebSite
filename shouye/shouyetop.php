affddd0bddf896e53f3fc5d2a0e2ccc4a87a1a21
lixingjia <873642712@qq.com>
2017-10-11 21:35:19 +0800

    shan chu

---
+++ shouye/shouyetop.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>无标题文档</title>
+<style type="text/css">
+body {
+	background-color: #1DFF83;
+}
+</style>
+</head>
+
+<body>
+<table width="1113" height="111" border="0">
+  <tr>
+    <td width="748" height="111"><img src="/MyPhpWebSite/login/image/川知.png" width="662" height="101" alt="川知欢迎您" /></td>
+    <td width="235">&nbsp;</td>
+    <td width="116">
+    <div>
+<?php
+    
+	echo username,'<br />';
+	echo '<a href="/MyPhpWebSite/login/my.php/">用户中心</a><br />';
+	//echo '点击此处 <a href="index.html?action=logout">注销</a> 登录！<br />';
+?>
+    </div>
+    </td>
+  </tr>
+</table>
+</body>
+</html>
